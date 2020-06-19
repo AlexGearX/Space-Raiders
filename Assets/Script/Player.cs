e0d81b15140b62953b739b0a3313632383441780
AlexGearX <alexgear34@gmail.com>
2020-06-20 01:29:45 +0200

    Add player movement/Shoot(click and stay click feature),Shredder for projectile

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Packages.Rider.Editor.UnitTesting;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Schema;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Scripting.APIUpdating;
 
@@ -10,8 +12,13 @@ public class Player : MonoBehaviour
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float paddingX = 1f;
     [SerializeField] float paddingY = 1f;
+    [SerializeField] GameObject LaserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileFiringPeriod = 1f;
 
     //Variable
+
+    Coroutine FiringCoroutine;
 
     float xMin;
     float xMax;
@@ -25,33 +32,49 @@ public class Player : MonoBehaviour
         playerSpaceLimit();
     }
 
-    private void playerSpaceLimit()
-    {
-        Camera gameCamera = Camera.main;
-        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + paddingX;
-        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - paddingX;
-        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + paddingY;
-        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - paddingY;
-    }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+           FiringCoroutine = StartCoroutine(FireContinuously());
+
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(FiringCoroutine);
+        }
+    }
+
+    IEnumerator FireContinuously()
+    {
+        while (true)
+        {
+            GameObject Laser = Instantiate(LaserPrefab, transform.position, Quaternion.identity) as GameObject;
+            Laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        }
     }
 
     private void Move()
     {
        //Command keyboard
 
-       var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-       var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+       //var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+       //var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
 
         // Mouse Command
 
-        //var deltaX = Input.GetAxis("Mouse X") * Time.deltaTime * moveSpeed;
-        //var deltaY = Input.GetAxis("Mouse Y") * Time.deltaTime * moveSpeed; 
+        var deltaX = Input.GetAxis("Mouse X") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Mouse Y") * Time.deltaTime * moveSpeed; 
 
         var newXPos = Mathf.Clamp((transform.position.x + deltaX), xMin, xMax);
         var newYPos = Mathf.Clamp((transform.position.y + deltaY), yMin, yMax);
@@ -61,5 +84,13 @@ public class Player : MonoBehaviour
         transform.position = new Vector2(newXPos, newYPos);
 
 
+    }
+    private void playerSpaceLimit()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + paddingX;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - paddingX;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + paddingY;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - paddingY;
     }
 }
