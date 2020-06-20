7970e69c2336b239078226511468ec3251186ad5
AlexGearX <alexgear34@gmail.com>
2020-06-20 22:09:53 +0200

    add ennemy path and wave

---
+++ Assets/Script/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> WaveConfigs;
+    int startingWave = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentWave = WaveConfigs[startingWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for (int enemyCount = 0 ; enemyCount < (waveConfig.GetNumberOfEnemies()); enemyCount++)
+        { 
+            var newEnemy = Instantiate(waveConfig.GetEnemyPrefab(), waveConfig.GetWayPoints()[0].transform.position, quaternion.identity);
+
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
+
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+            
+        }
+    }
+
+}
